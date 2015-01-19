0ad9d1cb413a64b46c0c4c136b645d1ea26bb87f
billyS <ws52@uni.brighton.ac.uk>
2015-01-19 20:19:18 +0000

    Adding Boot complete Receiver
    enables my service to be started when the device boots

---
+++ kidsafe+/src/com/example/uk/co/kidsafe/BootCompletedReceiver.java
@@ -0,0 +1,18 @@
+package com.example.uk.co.kidsafe;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    final static String TAG = "BootCompletedReceiver";
+
+    //starting service when device boots
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        Log.w(TAG, "Starting Location Service...");
+        context.startService(new Intent(context, LocationService.class));
+    }
+}
